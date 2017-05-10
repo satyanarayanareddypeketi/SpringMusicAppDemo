b4c47306388de3ccc074cd52d698c900b7db5a30
Abhinav Mishra <abhinavmi1@users.noreply.github.com>
2017-05-10 02:42:13 +0000

    Update HelloControllerTest.java

--- src/test/java/com/hello/controller/HelloControllerTest.java
+++ src/test/java/com/hello/controller/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.hello.controller;
+package com.hello.controller; 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
